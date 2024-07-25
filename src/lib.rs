8eb4e5942e63392eb1c8e8376ed08ca006d0e9b6
pyxel00 <pyxel1143@gmail.com>
2024-07-25 17:52:47 +0300

    Used rust-fmt to format

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 use std::{cmp, usize};
 
-const HEX_TABLE:[char; 16] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+const HEX_TABLE: [char; 16] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+];
 const BASE64_TABLE: [char; 64] = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' 
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9', '+', '/',
 ];
 
-pub struct Crypt {}
+pub struct Crypt;
 
 impl Crypt {
-    pub fn encode_16(input:&String) -> String {
+    pub fn encode_16(input: &String) -> String {
         let mut encoded = String::new();
 
         let vec = Self::split(&Self::str_to_binary(input), 4);
@@ -23,26 +25,26 @@ impl Crypt {
         encoded
     }
 
-    pub fn decode_16(input:&String) -> String {
+    pub fn decode_16(input: &String) -> String {
         let mut decoded = String::new();
         let mut binary = String::new();
 
         for ch in input.chars() {
             let decimal = HEX_TABLE.iter().position(|c| *c == ch).unwrap();
-            binary.push_str(&format!("{:04b}", decimal));  
-        };
+            binary.push_str(&format!("{:04b}", decimal));
+        }
 
         let vec = Self::split(&binary, 8);
 
         for item in vec {
-            let decimal = u8::from_str_radix(&item, 2).unwrap(); 
+            let decimal = u8::from_str_radix(&item, 2).unwrap();
             decoded.push(decimal as char);
-        };
+        }
 
         decoded
     }
 
-    pub fn encode_64(input:&String) -> String {
+    pub fn encode_64(input: &String) -> String {
         let mut encoded = String::new();
         let mut padding = String::new();
 
@@ -59,27 +61,27 @@ impl Crypt {
 
         encoded + &padding
     }
-    pub fn decode_64(input:&String) -> String {
-        let un_padded:String = input.chars().filter(|c| *c != '=').collect();
+    pub fn decode_64(input: &String) -> String {
+        let un_padded: String = input.chars().filter(|c| *c != '=').collect();
         let mut decoded = String::new();
         let mut binary = String::new();
 
         for ch in un_padded.chars() {
             let decimal = BASE64_TABLE.iter().position(|c| *c == ch).unwrap();
             binary.push_str(&format!("{:06b}", decimal));
-        };
+        }
 
         let vec = Self::split(&binary, 8);
 
         for item in vec {
             let decimal = u8::from_str_radix(&item, 2).unwrap();
             decoded.push(decimal as char);
-        };
+        }
 
         decoded
     }
 
-    fn str_to_binary(input:&String) -> String {
+    fn str_to_binary(input: &String) -> String {
         let mut binary = String::new();
 
         for char in input.chars() {
@@ -89,15 +91,15 @@ impl Crypt {
         binary
     }
 
-    fn split(input:&String, at:usize) -> Vec<String> {
+    fn split(input: &String, at: usize) -> Vec<String> {
         let mut last = Vec::new();
-        let mut i:usize = 0;
+        let mut i: usize = 0;
 
         while i != input.len() {
             let n_point = cmp::min(i + at, input.len());
             last.push(input[i..n_point].to_string());
             i = n_point;
-        };
+        }
 
         last
     }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 use clap::{Parser, Subcommand};
 use crypt::Crypt;
 
-
 #[derive(Parser)]
 struct Cli {
     #[command(subcommand)]
-    command:Option<Commands>
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
     Encode {
         #[arg(long)]
-        base64:bool,
+        base64: bool,
         #[arg(long)]
-        base16:bool,
-        input:String
+        base16: bool,
+        input: String,
     },
     Decode {
         #[arg(long)]
-        base64:bool,
+        base64: bool,
         #[arg(long)]
-        base16:bool,
-        input:String
-    }
+        base16: bool,
+        input: String,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Some(Commands::Encode { base64, base16 , input}) => {
+        Some(Commands::Encode {
+            base64,
+            base16,
+            input,
+        }) => {
             if *base16 {
                 let encoding = Crypt::encode_16(input);
                 println!("{encoding}")
@@ -39,7 +42,11 @@ fn main() {
                 println!("{encoding}")
             }
         }
-        Some(Commands::Decode { base64, base16, input }) => {
+        Some(Commands::Decode {
+            base64,
+            base16,
+            input,
+        }) => {
             if *base16 {
                 let decoding = Crypt::decode_16(input);
                 println!("{decoding}")
